ed1b981ab9a5083370647710803b16c739ae318f
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2022-01-15 18:07:17 +0100

    Read computer move js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+printMessage('Mój ruch to: ' + computerMove);
+
+else if(randomNumber == 2){
+	cmputerMove = "papier"
+}
+printMessage('Mój ruch to: ' + computerMove);
+
+else if (randomNumber == 3){
+	computerMove = "nożyce";
+
+printMessage('Mój ruch to: ' + computerMove);
